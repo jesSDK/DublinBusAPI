e12cc46f8c2ce216549ca01b9fd07e161f042288
Jessica <killzone135@gmail.com>
2016-09-15 00:10:45 +0100

    will clean up after strikes are over (17th?)

---
+++ DublinBusAPI/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace DublinBusAPI
+{
+    public class Class1
+    {
+        public class init
+        {
+            public static void doinit(string stopno){
+                //html magic
+                Console.WriteLine("Dublin Bus API Loaded...");
+                HtmlWeb hweb = new HtmlWeb();
+                Console.WriteLine("Connecting to RTPI...");
+                HtmlDocument doc = hweb.Load("http://www.rtpi.ie/Popup_Content/WebDisplay/WebDisplay.aspx?stopRef=" + stopno);
+                Console.WriteLine("Method 2: ");
+                foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//div[@class='gridContainer']/table"))
+                {
+                    Console.WriteLine("Found: " + table.Id);
+                    foreach (HtmlNode row in table.SelectNodes("tr"))
+                    {
+                        Console.WriteLine("row");
+                        foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                        {
+                            Console.WriteLine("cell: " + cell.InnerText);
+                        }
+                        Console.Read();
+                    }
+                }
+            }
+           
+        }
+    }
+}
