d1c6a3112b8a29c399fe35867f24b80f1ad348ac
Jessica <killzone135@gmail.com>
2016-09-17 12:25:12 +0100

    Strikes are over! Updated code to actually grab times now. Returns as a StringCollection

--- DublinBusAPI/Class1.cs
+++ DublinBusAPI/Class1.cs
@@ -5,37 +5,46 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using HtmlAgilityPack;
-
+using System.Collections.Specialized;
 namespace DublinBusAPI
 {
     public class Class1
     {
         public class init
         {
-            public static void doinit(string stopno){
+            public static StringCollection doinit(string stopno){
                 //html magic
                 Console.WriteLine("Dublin Bus API Loaded...");
                 HtmlWeb hweb = new HtmlWeb();
                 Console.WriteLine("Connecting to RTPI...");
                 HtmlDocument doc;
+                StringCollection results = new StringCollection();
                 try
                 {
-                     doc = hweb.Load("http://www.rtpi.ie/Text/WebDisplay.aspx?stopRef=" + stopno);
-                    Console.WriteLine("Method 2: ");
-                    foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//div[@class='gridContainer']/table"))
+                     doc = hweb.Load("http://www.rtpi.ie/Popup_Content/WebDisplay/WebDisplay.aspx?stopRef=" + stopno);
+                   // Console.WriteLine("Method 1: ");
+                    foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table[@class='rtiTable']"))
                     {
-                        Console.WriteLine("Found: " + table.Id);
                         foreach (HtmlNode row in table.SelectNodes("tr"))
                         {
-                            Console.WriteLine("row");
-                            foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                          //  Console.WriteLine();
+                            if(row.InnerText.Contains("Service Name"))
                             {
-                                Console.WriteLine("cell: " + cell.InnerText);
+
+                            }else
+                            {
+                                foreach (HtmlNode cell in row.SelectNodes("td|th"))
+                                {
+                                    //Console.Write(" " + cell.InnerText);
+                                    results.Add(" " + cell.InnerText);
+                                }
                             }
-                            Console.Read();
+                          
                         }
                     }
+                   
                 }
+
                 catch (Exception ex)
                 {
                     if (ex is System.NullReferenceException)
@@ -49,8 +58,8 @@ namespace DublinBusAPI
                         }
                     }
                 }
-                
-                 
+
+                return results;
             }
             public static void getRequest(string sURL)
             {
